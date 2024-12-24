5d265ed5e15e2eda1db509e937a44abb1639ca9c
Kirill Perelygin <perelygin.kirill@gmail.com>
2024-12-24 21:25:43 +0300

    Starting to work on randomizer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,64 +17,71 @@ public class Main {
         System.out.println("1. Я пользователь - введу UUID");
         System.out.println("2. Я новенький - зарегистрируйте меня");
         Scanner scanner = new Scanner(System.in);
+        boolean isBoolean = true;
+        while (isBoolean) {
         choiceValue = scanner.nextInt();
-        switch (choiceValue) {
-            case (1): {
-                System.out.println("Введи ранее выданный тебе UUID");
-                userUUID = scanner.next();
-                // TODO Придумать регуляроное выражение, потому что сейчас это строка и пускает по любому символу
-                try {
-                    if (UserDatabase.checkUserExistance(userUUID)) {
-                        System.out.println("С вовзращением!");
-                        System.out.println("Что хотите сделать?");
-                        while (true) {
-                            System.out.println("1. Введу ранее добавленную ссылку");
-                            System.out.println("2. Мне нужно сгенерировать ссылку");
-                            System.out.println("3. Хочу узнать все свои ссылки");
-                            System.out.println("4. Удалить ранее сгенерированную ссылку");
-                            secondChoiceValue = scanner.nextInt();
-                            switch (secondChoiceValue) {
-                                case (1): {
-                                    shortUrl = scanner.next();
-                                    longUrl = UserDatabase.getLongUrl(shortUrl);
-                                    UserURLs.visitShortUrl(longUrl);
-                                    UserDatabase.counterPlus(shortUrl);
-                                    // TODO добавить метод, который будет брать новый каунтер и выводить его как строку -> ваше количетсво переводов или типа того
-                                    UserDatabase.deleteMaxCounterRows();
+            switch (choiceValue) {
+                case (1): {
+                    System.out.println("Введи ранее выданный тебе UUID");
+                    userUUID = scanner.next();
+                    // TODO Придумать регуляроное выражение, потому что сейчас это строка и пускает по любому символу
+                    try {
+                        if (UserDatabase.checkUserExistance(userUUID)) {
+                            System.out.println("С вовзращением!");
+                            System.out.println("Что хотите сделать?");
+                            while (true) {
+                                System.out.println("1. Введу ранее добавленную ссылку");
+                                System.out.println("2. Мне нужно сгенерировать ссылку");
+                                System.out.println("3. Хочу узнать все свои ссылки");
+                                System.out.println("4. Удалить ранее сгенерированную ссылку");
+                                System.out.println("5. Хочу назад");
+                                secondChoiceValue = scanner.nextInt();
+                                switch (secondChoiceValue) {
+                                    case (1): {
+                                        shortUrl = scanner.next();
+                                        longUrl = UserDatabase.getLongUrl(shortUrl);
+                                        UserURLs.visitShortUrl(longUrl);
+                                        UserDatabase.counterPlus(shortUrl);
+                                        // TODO добавить метод, который будет брать новый каунтер и выводить его как строку -> ваше количетсво переводов или типа того
+                                        UserDatabase.deleteMaxCounterRows();
 
-                                    // TODO Метод, который получает из таблицы значение каунтера.
-                                    break;
-                                }
-                                case (2): {
-                                    System.out.println("Введите ссылку, которую нужно сократить");
-                                    longUrl = scanner.next();
-                                    shortUrl = UserURLs.createShortUrl(longUrl);
-                                    counterValue = UserURLs.getCounter(0);
-                                    UserDatabase.addingUserInfoToTheTable(userUUID, longUrl, shortUrl, counterValue);
-                                    break;
-                                }
-                                case (3):{
-                                    UserDatabase.selectAllUserUrls(userUUID);
-                                    break;
-                                }
-                                case (4):{
-                                    System.out.println("Введите ссылку, которую хотите удалить");
-                                    shortUrl = scanner.next();
-                                    UserDatabase.deletSelectedUrl(userUUID, shortUrl);
-                                    break;
+                                        // TODO Метод, который получает из таблицы значение каунтера.
+                                        break;
+                                    }
+                                    case (2): {
+                                        System.out.println("Введите ссылку, которую нужно сократить");
+                                        longUrl = scanner.next();
+                                        shortUrl = UserURLs.createShortUrl(longUrl);
+                                        counterValue = UserURLs.getCounter(0);
+                                        UserDatabase.addingUserInfoToTheTable(userUUID, longUrl, shortUrl, counterValue);
+                                        break;
+                                    }
+                                    case (3): {
+                                        UserDatabase.selectAllUserUrls(userUUID);
+                                        break;
+                                    }
+                                    case (4): {
+                                        System.out.println("Введите ссылку, которую хотите удалить");
+                                        shortUrl = scanner.next();
+                                        UserDatabase.deletSelectedUrl(userUUID, shortUrl);
+                                        break;
+                                    }
+                                    case (5): {
+                                        isBoolean = false;
+                                        break;
+                                    }
                                 }
                             }
+                        } else {
+                            System.out.println("Такого пользователя не знаем, попробуйте зарегистрироваться");
                         }
-                    } else {
-                        System.out.println("Такого пользователя не знаем, попробуйте зарегистрироваться");
+                        ;
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
                     }
-                    ;
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
+                    break;
                 }
-                break;
-            }
-            // TODO Проверка на наличие записи теперь есть, но даже при наличии ошибки идем дальше по процессу -> нужно понять как этого избежать
+                // TODO Проверка на наличие записи теперь есть, но даже при наличии ошибки идем дальше по процессу -> нужно понять как этого избежать
 
 
                 case (2): {
@@ -92,3 +99,4 @@ public class Main {
 
         }
     }
+}
--- src/main/java/UserURLs.java
+++ src/main/java/UserURLs.java
@@ -2,15 +2,16 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Random;
 
 public class UserURLs {
 
-    public static final String fixURL = "https://www.clck.ru/";
+    public static final String fixURL = "clck.ru/";
     public static String shortUrl;
 
     // TODO придумать метод, который будет рандомно генерировать 6 знаков UPPERlowerCaSe+numbers -> https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/
     public static String createShortUrl(String longUrl){
-        shortUrl = fixURL + "357";
+        shortUrl = fixURL + generateRandomString();
         System.out.println("Ваша сокращенная ссылка равняется " + shortUrl);
         return shortUrl;
     }
@@ -34,6 +35,31 @@ public class UserURLs {
             throw new RuntimeException(e);
         }
     }
+
+
+        public static String generateRandomString() {
+            // Создаем объект для генерации случайных чисел
+            Random random = new Random();
+
+            // Определяем диапазон символов от 'A' до 'Z' и от 'a' до 'z'
+            int rangeUpper = 'Z' - 'A';
+            int rangeLower = 'z' - 'a';
+
+            // Создаем строку длиной 5 символов
+            StringBuilder sb = new StringBuilder(5);
+
+            for (int i = 0; i < 5; i++) {
+                if (random.nextBoolean()) { // Случайный выбор между верхним и нижним регистром
+                    char ch = (char) ('A' + random.nextInt(rangeUpper));
+                    sb.append(ch); // Добавляем заглавную букву
+                } else {
+                    char ch = (char) ('a' + random.nextInt(rangeLower));
+                    sb.append(ch); // Добавляем строчную букву
+                }
+            }
+
+            return sb.toString(); // Преобразуем StringBuilder в строку и возвращаем результат
+        }
 
     public static void main(String[] args) {
 
